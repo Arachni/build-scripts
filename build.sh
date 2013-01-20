b1862489c8114b6a1d97cb25d0ed67a5ac1a9eb7
Tasos Laskos <tasos.laskos@gmail.com>
2013-01-20 19:02:22 +0200

    Upated to ruby-1.9.3-p374

--- build.sh
+++ build.sh
@@ -100,7 +100,7 @@ libs=(
     ftp://xmlsoft.org/libxslt/libxslt-1.1.28.tar.gz
     http://curl.haxx.se/download/curl-7.28.1.tar.gz
     https://rvm.io/src/yaml-0.1.4.tar.gz
-    http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p327.tar.gz
+    http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p374.tar.gz
 )
 
 #
