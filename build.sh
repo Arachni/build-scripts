cbba92dee8787f448df98d4883b4f39863fc8390
Tasos Laskos <tasos.laskos@gmail.com>
2015-04-01 17:22:57 +0300

    Upgraded Heimdal to compile on Linux

--- build.sh
+++ build.sh
@@ -84,7 +84,7 @@ libs=(
     http://www.openssl.org/source/openssl-1.0.1i.tar.gz
     http://www.sqlite.org/2014/sqlite-autoconf-3080500.tar.gz
     http://ftp.gnu.org/pub/gnu/ncurses/ncurses-5.9.tar.gz
-    http://www.h5l.org/dist/src/heimdal-1.4.tar.gz
+    http://www.h5l.org/dist/src/heimdal-1.4.1rc2.tar.gz
     http://curl.haxx.se/download/curl-7.41.0.tar.gz
     http://pyyaml.org/download/libyaml/yaml-0.1.4.tar.gz
     http://ftp.postgresql.org/pub/source/v9.4.1/postgresql-9.4.1.tar.gz
